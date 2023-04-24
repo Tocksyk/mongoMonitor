0be3a186d9fcc521f28ee7653f2909a7f05b5438
ils-sameed <s.ansari@thesynapses.com>
2023-04-24 18:08:54 +0530

    add code format

--- app/services/esCruds.js
+++ app/services/esCruds.js
@@ -1,65 +1,70 @@
-const esClient = require("../models/elasticsearchClient")
+const esClient = require("../models/elasticsearchClient");
 
 let esCrudsServices = {
-    deleteIndex: async (index) => {
-        esClient.indices.delete().then(data => {
-            console.log(`Index Deleted ${index}`);
-        }).catch(console.log)
-    },
-    insertData: async (data, index, typeStr) => {
+  deleteIndex: async (index) => {
+    esClient.indices
+      .delete()
+      .then((data) => {
+        console.log(`Index Deleted ${index}`);
+      })
+      .catch(console.log);
+  },
+  insertData: async (data, index, typeStr) => {
+    esClient
+      .index({
+        index: index,
+        type: typeStr,
+        body: data,
+        method: "post",
+      })
+      .then(console.log)
+      .catch(console.log);
+  },
+  readData: async (queryObj, index, typeObj) => {
+    esClient
+      .msearch({
+        index: index,
+        type: typeObj,
+        body: [{}, { query: queryObj }],
+      })
+      .then((data) => console.log(JSON.stringify(data, null, 2)))
+      .catch(console.log);
+  },
+  indexExist: async (index) => {
+    esClient.indices
+      .exists({
+        index: index,
+      })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch(console.log);
+  },
+  createIndex: async (index) => {
+    esClient.indices.create({ index: index }).then(console.log).catch(console.log);
+  },
+  readAllData: async () => {
+    esClient
+      .msearch({
+        body: [{}, { query: { match_all: {} } }],
+      })
+      .then((data) => console.log(JSON.stringify(data, null, 2)))
+      .catch(console.log);
+  },
+  deleteData: async (id, index, typeObj) => {
+    esClient
+      .deleteByQuery({
+        index: index,
+        type: typeObj,
+        body: {
+          query: {
+            match: { id: id },
+          },
+        },
+      })
+      .then(console.log)
+      .catch(console.log);
+  },
+};
 
-        esClient.index({
-            index: index,
-            type: typeStr,
-            body: data,
-            method: 'post'
-        }).then(console.log).catch(console.log);
-
-    },
-    readData: async (queryObj, index, typeObj) => {
-        esClient.msearch({
-            index: index,
-            type: typeObj,
-            body: [
-                {},
-                { query: queryObj }
-            ]
-        }).then(data => console.log(JSON.stringify(data, null, 2)))
-            .catch(console.log);
-    },
-    indexExist: async (index) => {
-        esClient.indices.exists({
-            index: index
-        }).then(data => {
-            console.log(data);
-        }).catch(console.log);
-    },
-    createIndex: async (index) => {
-        esClient.indices.create({ index: index })
-            .then(console.log)
-            .catch(console.log);
-    },
-    readAllData: async () => {
-        esClient.msearch({
-            body: [
-                {},
-                { query: { "match_all": {} } }
-            ]
-        }).then(data => console.log(JSON.stringify(data, null, 2))).catch(console.log);
-    },
-    deleteData: async (id, index, typeObj) => {
-        esClient.deleteByQuery({
-            index: index,
-            type: typeObj,
-            body: {
-                query: {
-                    match: { "id": id }
-                }
-            }
-        }).then(console.log).catch(console.log);
-    }
-}
-
-
-
-module.exports = esCrudsServices
+module.exports = esCrudsServices;
